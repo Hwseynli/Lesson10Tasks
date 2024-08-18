6cdc7e9f6cdb71a4fdd518c5cb245c12ef75de15
zeyneb <zybhuseynli@gmail.com>
2024-08-18 14:40:04 +0400

    commit

--- Models/Task1.cs
+++ Models/Task1.cs
@@ -4,6 +4,7 @@ namespace Lesson10Tasks.Models
     {
         public static void Task1Main(string fraction)
         {
+            //fration un doğruluğunu yoxlayaq
             if (string.IsNullOrEmpty(fraction) ||
                 !fraction.Contains("/") ||
                 fraction.Any(char.IsLetter) ||
@@ -33,13 +34,16 @@ namespace Lesson10Tasks.Models
 
         static string SimplifyFraction(string fraction)
         {
+             // Kəsri surət və məxrəcə bölək
             string[] parts = fraction.Split('/');
-            int numerator = int.Parse(parts[0]);
-            int denominator = int.Parse(parts[1]);
+            int numerator = int.Parse(parts[0]);// Surət
+            int denominator = int.Parse(parts[1]);// Məxrəc
             // ƏBOB (GCD)
             int gcd = GCD(numerator, denominator);
+            // Surət və məxrəci ƏBOB a bölmək
             numerator /= gcd;
             denominator /= gcd;
+             // Sadələşdirilmiş kəsri qaytaraq
             return $"{numerator}/{denominator}";
         }
 
--- obj/Debug/net6.0/Lesson10Tasks.AssemblyInfo.cs
+++ obj/Debug/net6.0/Lesson10Tasks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson10Tasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec2410326702d8c2bc4d63c2d1f03772891244a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson10Tasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson10Tasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
